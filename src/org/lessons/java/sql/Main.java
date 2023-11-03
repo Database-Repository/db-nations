80b37f8505291294c835b232cd7fbcc1205f1942
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-03 15:18:22 +0100

    Commit 2 - add connection

--- src/org/lessons/java/sql/Main.java
+++ src/org/lessons/java/sql/Main.java
@@ -1,10 +1,27 @@
 package org.lessons.java.sql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        //parametri di connesione
+        String url = "jdbc:mysql://localhost:3306/db_nations";
+        String user = "root";
+        String password = "root";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)){
+
+        }catch (SQLException e) {
+            System.out.println("Unable to open connection");
+            e.printStackTrace();
+        }
+
+        scanner.close();
     }
 
 }
