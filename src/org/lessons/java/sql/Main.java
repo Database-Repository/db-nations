684b3d653eb91ec2b2271e0c4b896db31e543eac
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-03 14:46:51 +0100

    Commit 1 - first commit

---
+++ src/org/lessons/java/sql/Main.java
@@ -0,0 +1,10 @@
+package org.lessons.java.sql;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
+}
