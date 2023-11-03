fca2386e83b068aa7482147827db60e30cc6bd99
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-03 17:12:20 +0100

    Commit 4 - milestone 3

--- src/org/lessons/java/sql/Main.java
+++ src/org/lessons/java/sql/Main.java
@@ -14,14 +14,21 @@ public class Main {
 
         try (Connection connection = DriverManager.getConnection(url, user, password)){
 
+            System.out.print("Name of country: ");
+            String searchString = scanner.nextLine();
+
             String query =
                     "SELECT  c.country_id, c.name, r.name, c2.name "
                     + "FROM countries c "
                     + "JOIN regions r  ON r.region_id  =  c.region_id "
                     + "JOIN continents c2 ON c2.continent_id = r.continent_id "
+                    + "WHERE c.name LIKE ? "
                     + "ORDER BY c.name ";
 
             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+                preparedStatement.setString(1, '%' + searchString + '%');
+
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
                     while (resultSet.next()){
                        int countryId        = resultSet.getInt("country_id");
@@ -29,7 +36,7 @@ public class Main {
                        String regionName    = resultSet.getString("r.name");
                        String continentName = resultSet.getString("c2.name");
 
-                        System.out.println(countryId + " || " + countryName + " || " + regionName + " - " + continentName);
+                        System.out.println(countryId + " || " + countryName + " || " + regionName + " || " + continentName);
                     }
                 } catch (SQLException e) {
                     System.out.println("Unable to execute query");
