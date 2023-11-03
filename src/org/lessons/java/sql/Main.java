51942f4b983bf4a3a2571aa034912ba3253c6ddf
Paolo Falco <paolo.falcoapp@gmail.com>
2023-11-03 16:58:43 +0100

    Commit 3 - milestone 2

--- src/org/lessons/java/sql/Main.java
+++ src/org/lessons/java/sql/Main.java
@@ -1,23 +1,43 @@
 package org.lessons.java.sql;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //parametri di connesione
+        //parametri di connessione
         String url = "jdbc:mysql://localhost:3306/db_nations";
         String user = "root";
         String password = "root";
 
         try (Connection connection = DriverManager.getConnection(url, user, password)){
 
-        }catch (SQLException e) {
-            System.out.println("Unable to open connection");
+            String query =
+                    "SELECT  c.country_id, c.name, r.name, c2.name "
+                    + "FROM countries c "
+                    + "JOIN regions r  ON r.region_id  =  c.region_id "
+                    + "JOIN continents c2 ON c2.continent_id = r.continent_id "
+                    + "ORDER BY c.name ";
+
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()){
+                       int countryId        = resultSet.getInt("country_id");
+                       String countryName   = resultSet.getString("c.name");
+                       String regionName    = resultSet.getString("r.name");
+                       String continentName = resultSet.getString("c2.name");
+
+                        System.out.println(countryId + " || " + countryName + " || " + regionName + " - " + continentName);
+                    }
+                } catch (SQLException e) {
+                    System.out.println("Unable to execute query");
+                    e.printStackTrace();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Unable to prepare statement");
             e.printStackTrace();
         }
 
